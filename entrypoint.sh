a4a3591586e83d71ac0492cec293c686cff7ddf6
Aaron Steele <eightysteele@gmail.com>
2024-05-24 09:16:20 -0700

    compose, x, emacs working

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,20 @@
 
 git config --global --add safe.directory "*"
 
-yes | emacs --debug-init --daemon
+if pgrep -af "^emacs.*--daemon$" >/dev/null; then
+	echo "Emacs daemon is running."
+else
+	echo "Starting Emacs daemon."
+	yes | emacs --debug-init --daemon
+fi
+
+omd="$XDG_CONFIG_HOME/org-roam-data"
+omdv="$XDG_CONFIG_HOME/emacs/org-roam/"
+if [ -d "$omd" ]; then
+	echo "First run: moving $omd into $omdv volume"
+	mv "$omd" "$omdv"
+fi
 
 /bin/bash
+
+#exec "@"
